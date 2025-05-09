77c963357f33da949d196a016b5a5c9501b2471b
Austin Johnson <ajohnson132490@gmail.com>
2025-05-09 09:10:05 -0500

    Abstracted testing stuff and manual ticket grabbing

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ please = f'{email}/token', api_token
 ## --Cleaning up previous output-- ##
 if os.path.exists("output.txt"):
     os.remove("output.txt")
+if os.path.exists("json.txt"):
+    os.remove("json.txt")
 
 ## --Setting the query-- ##
 options = h.printMenu()
@@ -38,7 +40,6 @@ params = {
 ## --Searching-- ##
 tickets = []
 try:
-    
     # Getting the first page of data
     response = requests.get(search_url, params = params, auth = please)
     if response.status_code == 429:
@@ -65,11 +66,10 @@ except:
 
 print(f"{len(tickets)} tickets found.")
 
-# Writing the data to a text file for storage
-#json_string = json.dumps(response.json(), indent=2)
-#with open('json.txt', mode='w', encoding='utf-8') as f:
-#    f.write(json_string)
+## --Uncomment this line to see raw JSON-- ##
+#h.outputLastPageToTxt(response)
 
+## --Formatting and writing all data to the output file-- ##
 with open('output.txt', mode='w', encoding='utf-8') as f:
     with tqdm(range(0,100), total=len(tickets), ncols = 100, desc="Writing Tickets") as progress_bar:
         for ticket in tickets:
@@ -81,26 +81,7 @@ with open('output.txt', mode='w', encoding='utf-8') as f:
             f.write("\n\n\n\n\n--------------------------END TICKET--------------------------\n\n\n\n\n")
             progress_bar.update(1)
 
-
-# Manually reading all tickets from an export of all tickets
-#with open('FILENAME', mode='r', encoding='utf-8') as f:
-#    data = [json.loads(line) for line in f]
-
-# Manual writing
-#with open('output1.txt', mode='w', encoding='utf-8') as f:
-#    for i in range(len(data)):
-#        if (data[i]["group"]["name"] == "GROUPNAME"):
-#            try:
-#                f.write(f"TITLE: {data[i]["subject"]}\n\n\nTICKET NUMBER: {data[i]["id"]},  CREATED AT: {data[i]["created_at"]}, LAST UPDATED: {data[i]["updated_at"]}\
-#                        \nREQUESTER: {data[i]["requester"]["name"]} ASSIGNEE: {data[i]["assignee"]["name"]}\
-#                        \nTICKET FORM: {h.searchForTicketFormById(data[i]["ticket_form_id"], please)}\nPRIORITY: {data[i]["priority"]}\
-#                        \nSTATUS: {data[i]["status"]}\nTAGS: {data[i]["tags"]}\nSATISFACTION RATING: {data[i]["satisfaction_rating"]}\n\n")
-#            except:
-#                f.write(f"TITLE: {data[i]["subject"]}\n\n\nTICKET NUMBER: {data[i]["id"]},  CREATED AT: {data[i]["created_at"]}, LAST UPDATED: {data[i]["updated_at"]}\
-#                        \nREQUESTER: {data[i]["requester"]["name"]} ASSIGNEE: Permanently deleted user\
-#                        \nTICKET FORM: {h.searchForTicketFormById(data[i]["ticket_form_id"], please)}\nPRIORITY: {data[i]["priority"]}\
-#                        \nSTATUS: {data[i]["status"]}\nTAGS: {data[i]["tags"]}\nSATISFACTION RATING: {data[i]["satisfaction_rating"]}\n\n")
-#            f.write(h.getAllComments(data[i]["comments"], please))
-#            f.write("\n\n\n\n\n--------------------------END TICKET--------------------------\n\n\n\n\n")
+## --Uncomment to manually search for all tickets in a group using a file-- ##
+#h.manuallySearchTickets(FILENAME, GROUP, please)
             
 print ("Output complete!")
